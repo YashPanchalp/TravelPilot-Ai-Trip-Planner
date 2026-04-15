721328e6e276b11f1891c83d765c31b81db2b117
Yash Panchal <776yashp@gmail.com>
2026-04-15 13:04:49 +0530

    Model Changed

--- src/service/AIModel.jsx
+++ src/service/AIModel.jsx
@@ -48,7 +48,7 @@ const apiKey = import.meta.env.VITE_GOOGLE_GENAI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-3.1-flash-lite-preview",
+  model: "gemini-3-flash-preview",
 });
 
 const generationConfig = {
